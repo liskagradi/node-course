8fb4a1d6b7d1e066e684244eeb2581b2c5d36e6d
Liska Loaiza <liska.loaiza@gradiweb.com>
2023-02-03 17:38:16 -0500

    Final webserver

--- webserver/app.js
+++ webserver/app.js
@@ -1,17 +1,52 @@
 import express from 'express';
+import { fileURLToPath } from 'url';
+import hbs from "hbs";
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+// DIR
+import path from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 const app = express();
+const port = process.env.PORT;
+
+// HBS
+
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
 
 //SERVIR CONTENIDO ESTATTICO
 
 app.use( express.static('public') )
 
-app.get('/hola-mundo', function (req, res) {
-  res.send('Hola mundo en su respectuva ruta')
-})
+
+// -- 
+app.get('/', function (req, res) {
+  res.render('home', {
+    titulo: 'Curso Node',
+    nombre: 'Liska Loaiza'
+  });
+});
+
+
+app.get('/generic', function (req, res) {
+  res.render('generic', {
+    titulo: 'Curso Node',
+    nombre: 'Liska Loaiza'
+  });
+});
+app.get('/elements', function (req, res) {
+  res.render('elements', {
+    titulo: 'Curso Node',
+    nombre: 'Liska Loaiza'
+  });
+});
 
 app.get('*', function (req, res) {
-  res.send('404')
-})
+  res.sendFile( __dirname +'/public/404.html')
+});
 
-app.listen(8080)
+app.listen(port)
